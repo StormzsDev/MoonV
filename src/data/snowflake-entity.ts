5085958979e6514e14f8e1a791048b89ae71fc99
damehamz <sulaimanakif@gmail.com>
2021-03-24 13:14:49 +0800

    web

--- src/data/snowflake-entity.ts
+++
@@ -1,3 +0,0 @@
-export default interface SnowflakeEntity {
-  id: string;
-}
--- src/modules/auto-mod/validators/content-validator.ts
+++
@@ -1,8 +0,0 @@
-import { GuildDocument, MessageFilter } from '../../../data/models/guild';
-import AutoMod from '../auto-mod';
-
-export interface ContentValidator {
-  filter: MessageFilter;
-  
-  validate(autoMod: AutoMod, content: string, guild: GuildDocument): void | Promise<void>;
-}
